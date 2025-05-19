fd7424d12772411ea7182893ef21b3214f6e8ccb
Amir Hossein <amir.h.mirzaei9@gmail.com>
2025-05-19 13:32:33 +0330

    Refactor MadClock to class-based structure

--- main.py
+++ main.py
@@ -2,15 +2,25 @@ from datetime import datetime
 from clock_modes.morse import num_morse_code
 
 
-def MadClock():
-    time = datetime.now().strftime("%H:%M")
-    mode_type = input("Which Mode : ").lower()
-    if mode_type == "morse":
-        output = list()
-        for x in time:
-            if x in num_morse_code:
-                output.append(num_morse_code[x])
-            elif x == ":":
+class MadClock:
+    def __init__(self):
+        self.time = datetime.now().strftime("%H:%M")
+
+    def run(self):
+        mode_type = input("Which Mode: ").lower()
+        if mode_type == "morse":
+            self.show_morse()
+
+    def show_morse(self):
+        output = []
+        for char in self.time:
+            if char in num_morse_code:
+                output.append(num_morse_code[char])
+            elif char == ":":
                 output.append("/")
         print(" ".join(output))
-MadClock()
+
+
+if __name__ == "__main__":
+    clock = MadClock()
+    clock.run()
