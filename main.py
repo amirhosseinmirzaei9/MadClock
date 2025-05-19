a1bbc1036d56f917d0e8a033c48fdef6fad5d92e
Amir Hossein <amir.h.mirzaei9@gmail.com>
2025-05-19 15:04:32 +0330

    Get dynamic time

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from clock_modes.morse import num_morse_code
 
 
 class MadClock:
-    def __init__(self):
-        self.time = datetime.now().strftime("%H:%M")
+    def get_time(self):
+        return datetime.now().strftime("%H:%M")
 
     def run(self):
         while True:
@@ -21,30 +21,29 @@ class MadClock:
             else:
                 print("Invalid mode. Please choose from: morse, binary, reverse.")
 
-
-
     def show_morse(self):
+        time = self.get_time()
         output = []
-        for char in self.time:
+        for char in time:
             if char in num_morse_code:
                 output.append(num_morse_code[char])
             elif char == ":":
                 output.append("/")
         print(" ".join(output))
 
-
     def show_binary(self):
-        output = list()
-        for char in self.time:
+        time = self.get_time()
+        output = []
+        for char in time:
             if char.isdigit():
-                output.append(bin(int(char))[2:])
+                output.append(format(int(char), "04b"))
             elif char == ":":
                 output.append("/")
         print(" ".join(output))
 
-
     def show_reverse(self):
-        hour, minute = self.time.split(":")
+        time = self.get_time()
+        hour, minute = time.split(":")
         reversed_hour = hour[::-1]
         reversed_minute = minute[::-1]
         print(f"{reversed_minute}:{reversed_hour}")
