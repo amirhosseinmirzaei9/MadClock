1515c59fa6e1204f79413a452042ee6f20143023
Amir Hossein <amir.h.mirzaei9@gmail.com>
2025-05-19 13:59:44 +0330

    Add reverse mode to MadClock

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ class MadClock:
             self.show_morse()
         elif mode_type == "binary":
             self.show_binary()
+        elif mode_type == "reverse":
+            self.show_reverse()
 
     def show_morse(self):
         output = []
@@ -22,6 +24,7 @@ class MadClock:
                 output.append("/")
         print(" ".join(output))
 
+
     def show_binary(self):
         output = list()
         for char in self.time:
@@ -30,6 +33,14 @@ class MadClock:
             elif char == ":":
                 output.append("/")
         print(" ".join(output))
+
+
+    def show_reverse(self):
+        hour, minute = self.time.split(":")
+        reversed_hour = hour[::-1]
+        reversed_minute = minute[::-1]
+        print(f"{reversed_minute}:{reversed_hour}")
+
 
 if __name__ == "__main__":
     clock = MadClock()
